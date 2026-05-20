aff31d87c257d698110e5620b807cb1a141d557b
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2026-05-20 17:26:51 +0200

    Update

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,10 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["Auth"]
-)
+from app.db.database import SessionLocal
+from app.models.user import User
+from app.schemas.auth_schema import RegisterSchema
+from app.core.security import hash_password
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/")
 def auth_test():
     return {"message": "Auth route works"}
+
+@router.post("/register")
+def register(user: RegisterSchema, db: Session = Depends(get_db)):
+    hashed_pw = hash_password(user.password)
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hashed_pw,
+        role_id=user.role_id
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created successfully"}
