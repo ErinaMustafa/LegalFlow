fb0377deefe405b57c7c921bc13f7389d260a46f
Erisa Sollova <erisasollova@gmail.com>
2026-05-20 18:15:13 +0200

    Krijimi i Login

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -6,6 +6,9 @@ from app.models.user import User
 from app.schemas.auth_schema import RegisterSchema
 from app.core.security import hash_password
 
+from fastapi import HTTPException
+from app.schemas.auth_schema import RegisterSchema, LoginSchema
+from app.core.security import hash_password, verify_password, create_access_token
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
@@ -37,3 +40,20 @@ def register(user: RegisterSchema, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"message": "User created successfully"}
+
+@router.post("/login")
+def login(user: LoginSchema, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid password")
+
+    token = create_access_token({"sub": db_user.email})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
