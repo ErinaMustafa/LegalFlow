d3cdfbf652b2e2bc5e4b576701cf07a4a61ccc38
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2026-05-26 11:32:27 +0200

    Krijimi i Login page

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState } from "react";
+import { loginUser } from "../api/authApi";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  const login = async (email, password) => {
+    const data = await loginUser({ email, password });
+
+    localStorage.setItem("token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data));
+
+    setUser(data);
+    return data;
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
